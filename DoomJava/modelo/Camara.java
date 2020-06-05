797c9711781478ccf558febcebde03515fa9630f
davidgfb <davidgracia99@gmail.com>
2020-06-05 16:29:10 +0200

    4

--- DoomJava/modelo/Camara.java
+++ DoomJava/modelo/Camara.java
@@ -5,109 +5,111 @@ import java.awt.event.KeyListener;
 
 
 public class Camara implements KeyListener{
-	public double xPos, 
-                      yPos, 
-                      xDir, 
-                      yDir, 
-                      xPlane, 
-                      yPlane;
-        
-	public boolean left, 
-                       right, 
-                       forward, 
-                       back;
-        
-	public final double MOVE_SPEED = 0.08;
-	public final double ROTATION_SPEED = 0.045;
-        
-	public Camara(double x, double y, double xd, double yd, double xp, double yp) {
-		this.setPosX(x);
-		this.setPosY(y);
-		this.setDirX(xd);
-		this.setDirY(yd);
-		this.setPlanoX(xp);
-		this.setPlanoY(yp);
-	}
-        
-        void setPosX(double POSX) {
-            this.xPos=POSX;
+    public double xPos, 
+                  yPos, 
+                  xDir, 
+                  yDir, 
+                  xPlane, 
+                  yPlane;
+
+    public boolean left, 
+                   right, 
+                   forward, 
+                   back;
+
+    public final double MOVE_SPEED = 0.08;
+    public final double ROTATION_SPEED = 0.045;
+
+    public Camara(double x, double y, double xd, double yd, double xp, double yp) {
+        this.setPosX(x);
+        this.setPosY(y);
+        this.setDirX(xd);
+        this.setDirY(yd);
+        this.setPlanoX(xp);
+        this.setPlanoY(yp);
+    }
+
+    void setPosX(double POSX) {
+        this.xPos=POSX;
+    }
+
+    void setPosY(double POSY) {
+        this.yPos=POSY;
+    }
+
+    void setDirX(double DIRX) {
+        this.xDir=DIRX;
+    }
+
+    void setDirY(double DIRY) {
+        this.yDir=DIRY;
+    }
+
+    void setPlanoX(double PLANOX) {
+        this.xPlane=PLANOX;
+    }
+
+    void setPlanoY(double PLANOY) {
+        this.yPlane=PLANOY;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent key) {
+        if((key.getKeyCode() == KeyEvent.VK_LEFT))
+                left = true;
+        if((key.getKeyCode() == KeyEvent.VK_RIGHT))
+                right = true;
+        if((key.getKeyCode() == KeyEvent.VK_UP))
+                forward = true;
+        if((key.getKeyCode() == KeyEvent.VK_DOWN))
+                back = true;
+    }
+    
+    @Override
+    public void keyReleased(KeyEvent key) {
+        if((key.getKeyCode() == KeyEvent.VK_LEFT))
+                left = false;
+        if((key.getKeyCode() == KeyEvent.VK_RIGHT))
+                right = false;
+        if((key.getKeyCode() == KeyEvent.VK_UP))
+                forward = false;
+        if((key.getKeyCode() == KeyEvent.VK_DOWN))
+                back = false;
+    }
+    
+    public void update(int[][] map) {
+        if(forward) {
+                if(map[(int)(xPos + xDir * MOVE_SPEED)][(int)yPos] == 0) {
+                        xPos+=xDir*MOVE_SPEED;
+                }
+                if(map[(int)xPos][(int)(yPos + yDir * MOVE_SPEED)] ==0)
+                        yPos+=yDir*MOVE_SPEED;
         }
-        
-        void setPosY(double POSY) {
-            this.yPos=POSY;
+        if(back) {
+                if(map[(int)(xPos - xDir * MOVE_SPEED)][(int)yPos] == 0)
+                        xPos-=xDir*MOVE_SPEED;
+                if(map[(int)xPos][(int)(yPos - yDir * MOVE_SPEED)]==0)
+                        yPos-=yDir*MOVE_SPEED;
         }
-        
-        void setDirX(double DIRX) {
-            this.xDir=DIRX;
+        if(right) {
+                double oldxDir=xDir;
+                xDir=xDir*Math.cos(-ROTATION_SPEED) - yDir*Math.sin(-ROTATION_SPEED);
+                yDir=oldxDir*Math.sin(-ROTATION_SPEED) + yDir*Math.cos(-ROTATION_SPEED);
+                double oldxPlane = xPlane;
+                xPlane=xPlane*Math.cos(-ROTATION_SPEED) - yPlane*Math.sin(-ROTATION_SPEED);
+                yPlane=oldxPlane*Math.sin(-ROTATION_SPEED) + yPlane*Math.cos(-ROTATION_SPEED);
         }
-        
-        void setDirY(double DIRY) {
-            this.yDir=DIRY;
+        if(left) {
+                double oldxDir=xDir;
+                xDir=xDir*Math.cos(ROTATION_SPEED) - yDir*Math.sin(ROTATION_SPEED);
+                yDir=oldxDir*Math.sin(ROTATION_SPEED) + yDir*Math.cos(ROTATION_SPEED);
+                double oldxPlane = xPlane;
+                xPlane=xPlane*Math.cos(ROTATION_SPEED) - yPlane*Math.sin(ROTATION_SPEED);
+                yPlane=oldxPlane*Math.sin(ROTATION_SPEED) + yPlane*Math.cos(ROTATION_SPEED);
         }
-        
-        void setPlanoX(double PLANOX) {
-            this.xPlane=PLANOX;
-        }
-        
-        void setPlanoY(double PLANOY) {
-            this.yPlane=PLANOY;
-        }
-        
-        @Override
-	public void keyPressed(KeyEvent key) {
-		if((key.getKeyCode() == KeyEvent.VK_LEFT))
-			left = true;
-		if((key.getKeyCode() == KeyEvent.VK_RIGHT))
-			right = true;
-		if((key.getKeyCode() == KeyEvent.VK_UP))
-			forward = true;
-		if((key.getKeyCode() == KeyEvent.VK_DOWN))
-			back = true;
-	}
-        @Override
-	public void keyReleased(KeyEvent key) {
-		if((key.getKeyCode() == KeyEvent.VK_LEFT))
-			left = false;
-		if((key.getKeyCode() == KeyEvent.VK_RIGHT))
-			right = false;
-		if((key.getKeyCode() == KeyEvent.VK_UP))
-			forward = false;
-		if((key.getKeyCode() == KeyEvent.VK_DOWN))
-			back = false;
-	}
-	public void update(int[][] map) {
-		if(forward) {
-			if(map[(int)(xPos + xDir * MOVE_SPEED)][(int)yPos] == 0) {
-				xPos+=xDir*MOVE_SPEED;
-			}
-			if(map[(int)xPos][(int)(yPos + yDir * MOVE_SPEED)] ==0)
-				yPos+=yDir*MOVE_SPEED;
-		}
-		if(back) {
-			if(map[(int)(xPos - xDir * MOVE_SPEED)][(int)yPos] == 0)
-				xPos-=xDir*MOVE_SPEED;
-			if(map[(int)xPos][(int)(yPos - yDir * MOVE_SPEED)]==0)
-				yPos-=yDir*MOVE_SPEED;
-		}
-		if(right) {
-			double oldxDir=xDir;
-			xDir=xDir*Math.cos(-ROTATION_SPEED) - yDir*Math.sin(-ROTATION_SPEED);
-			yDir=oldxDir*Math.sin(-ROTATION_SPEED) + yDir*Math.cos(-ROTATION_SPEED);
-			double oldxPlane = xPlane;
-			xPlane=xPlane*Math.cos(-ROTATION_SPEED) - yPlane*Math.sin(-ROTATION_SPEED);
-			yPlane=oldxPlane*Math.sin(-ROTATION_SPEED) + yPlane*Math.cos(-ROTATION_SPEED);
-		}
-		if(left) {
-			double oldxDir=xDir;
-			xDir=xDir*Math.cos(ROTATION_SPEED) - yDir*Math.sin(ROTATION_SPEED);
-			yDir=oldxDir*Math.sin(ROTATION_SPEED) + yDir*Math.cos(ROTATION_SPEED);
-			double oldxPlane = xPlane;
-			xPlane=xPlane*Math.cos(ROTATION_SPEED) - yPlane*Math.sin(ROTATION_SPEED);
-			yPlane=oldxPlane*Math.sin(ROTATION_SPEED) + yPlane*Math.cos(ROTATION_SPEED);
-		}
-	}
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
+    public void keyTyped(KeyEvent arg0) {
+        // TODO Auto-generated method stub
+
+    }
 }
--- DoomJava/modelo/Textura.java
+++ DoomJava/modelo/Textura.java
@@ -5,30 +5,49 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Textura {
-	public int[] pixels;
-	private String loc;
-	public final int SIZE;
-        
-        public static Textura wood = new Textura("/texturas/madera.png", 64);
-	public static Textura brick = new Textura("/texturas/ladrilloRojo.png", 64);
-	public static Textura bluestone = new Textura("/texturas/piedraAzul.png", 64);
-	public static Textura stone = new Textura("/texturas/piedraGris.png", 64);
-	
-	public Textura(String location, int size) {
-		loc = location;
-		SIZE = size;
-		pixels = new int[SIZE * SIZE];
-		load();
-	}
-	
-	private void load() {
-		try {
-			BufferedImage image = ImageIO.read(getClass().getResource(loc));
-			int w = image.getWidth();
-			int h = image.getHeight();
-			image.getRGB(0, 0, w, h, pixels, 0, w);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public int[] pixels;
+    private String loc;
+    public final int SIZE;
+
+    public static Textura wood = new Textura("/texturas/madera.png", 64),
+                          brick = new Textura("/texturas/ladrilloRojo.png", 64),
+                          bluestone = new Textura("/texturas/piedraAzul.png", 64),
+                          stone = new Textura("/texturas/piedraGris.png", 64);
+
+    public Textura(String location, int size) {
+        //this.setRuta(loc);
+        loc=location;
+        SIZE=size;
+        //this.setTamaño(size);
+        pixels=new int[SIZE * SIZE];
+        //this.setPixeles(new int[SIZE * SIZE]);
+        load();
+    }
+    
+    /*
+    void setRuta(String RUTA) {
+        this.loc=RUTA;
+    }
+    
+    
+    void setTamaño(int TAMAÑO) {
+        //this.SIZE=TAMAÑO;
+    }
+    
+    
+    void setPixeles(int[] PIXELES) {
+        this.pixels=PIXELES;
+    }
+    */
+    
+    private void load() {
+        try {
+            BufferedImage image = ImageIO.read(getClass().getResource(loc));
+            int w = image.getWidth();
+            int h = image.getHeight();
+            image.getRGB(0, 0, w, h, pixels, 0, w);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
